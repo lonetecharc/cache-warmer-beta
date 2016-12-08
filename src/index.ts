c62ca4a66bae99d211d6e2d4e2954b52f912f959
lonetecharc <bappaditya.bandyopadhyay@cognizant.com>
2016-12-08 19:13:34 +0530

    Improved ReadMe and logging

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { CollectionDetails } from './model/collectiondetails';
 import { ShowList } from './lib/showlist';
 import { ShowCollectionDetails } from './lib/showcollectiondetails';
 import {Promise} from 'es6-promise';
+import * as fs from 'fs';
 var list = new ShowList(
     config.get('API.park.urlPUB7').toString(),
     config.get('APISignature.globalnavShows').toString()
@@ -16,8 +17,11 @@ var list = new ShowList(
 let showlist : Promise<Show[]>;
 showlist = list.makeCall();
 
+fs.writeFileSync(__dirname + '/cachewarmlog.txt','Satrting Cache Warm at ' + new Date().toJSON().slice(0,10) + '\n');
+
 showlist.then(function(d){
     console.log('********************Warming started*****************');
+    fs.appendFile(__dirname + '/cachewarmlog.txt','got show list' + '\n');
     d.forEach(function(show){
         console.log(show.urlAlias);
         let showCollectionDetails = new ShowCollectionDetails(
@@ -30,6 +34,7 @@ showlist.then(function(d){
 
         collectionList.then(function(d){
             console.log(d);
+            fs.appendFile(__dirname + '/cachewarmlog.txt', show.urlAlias + ' warmed:: ' + JSON.stringify(d) + '\n');
         });
     })
 });
